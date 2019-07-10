6402313ad4615747be1128c331208fd0e14395bc
Learn <Learn@learn.com>
2019-07-10 23:43:26 +0700

    minor changes

--- 3.6.9 - Test_items/test_items.py
+++ 3.6.9 - Test_items/test_items.py
@@ -4,5 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_AddToBasketButton(browser):
 	browser.get(link)
-	time.sleep(7)#Время на проверку текста в кнопке на разных локалях
+	#time.sleep(7)# Время на проверку текста в кнопке на разных локалях для проверяющего, осталось только разкомментить :)
 	browser.find_element_by_css_selector(".btn-add-to-basket") #ищет кнопку добавления в корзину
+	assert button.is_displayed(), "The 'Add to basket' button isn't displayed"# Дополнительно проверяет видимость
